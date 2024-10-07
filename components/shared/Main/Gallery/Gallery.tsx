24cdc1312b3e1692de48709f9e2df6484e2248fc
Abu <dudumikah@gmail.com>
2024-10-07 11:05:30 +0500

    feat: update: gallery

--- components/shared/Main/Gallery/Gallery.tsx
+++ components/shared/Main/Gallery/Gallery.tsx
@@ -14,13 +14,46 @@ export const Gallery = ({ className }: Props) => {
             <h3 className='text-3xl lg:text-4xl font-bold'>
                 Галерея
             </h3>
-            <div className="grid grid-cols-1 mdx:grid-cols-2 lgx:grid-cols-3">
-                <Image
-                  src
-                />
+            <div className="grid grid-cols-1 mdx:grid-cols-2 lgx:grid-cols-3 gap-4">
+              <Image
+                src="/images/main/gallery/image (1).png"
+                width={1000}
+                height={1000}
+                alt="Gallery Image 1"
+              />
+              <Image
+                src="/images/main/gallery/image (1).png"
+                width={1000}
+                height={1000}
+                alt="Gallery Image 1"
+              />
+              <Image
+                src="/images/main/gallery/image (1).png"
+                width={1000}
+                height={1000}
+                alt="Gallery Image 1"
+              />
+              <Image
+                src="/images/main/gallery/image (1).png"
+                width={1000}
+                height={1000}
+                alt="Gallery Image 1"
+              />
+              <Image
+                src="/images/main/gallery/image (1).png"
+                width={1000}
+                height={1000}
+                alt="Gallery Image 1"
+              />
+              <Image
+                src="/images/main/gallery/image (1).png"
+                width={1000}
+                height={1000}
+                alt="Gallery Image 1"
+              />
             </div>
             <div className='w-full flex items-center justify-center'>
-                <Button className="px-8 py-4 ">
+                <Button className="px-8 py-4 rounded-full text-xl">
                 Загрузить еще
                 </Button>
             </div>
