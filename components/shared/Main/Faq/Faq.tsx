43a10cd1de785800aaea48d45fa3f3a63e4a9bc9
Faizullin Abdukhakim <dudumikah@gmail.com>
2024-10-08 18:06:16 +0500

    feat: finish

--- components/shared/Main/Faq/Faq.tsx
+++ components/shared/Main/Faq/Faq.tsx
@@ -12,28 +12,33 @@ interface Props {
   className?: string;
 }
 
-export const Faq = ({ className }: Props) => {
-  const t = useTranslations("Main.Faq"); // Подключаемся к разделу "Faq"
-
-  const questions = t.raw("questions"); // Получаем список вопросов и ответов
-
-  return (
-    <section className={cn("bg-white py-24", className)}>
-      <div className="w-full max-w-[1500px] px-4 mx-auto flex flex-col gap-12">
-        <h3 className="text-3xl lg:text-4xl font-bold">{t("title")}</h3>
-        <Accordion type="single" collapsible className="w-full">
-          {questions.map((item: any, index: number) => (
-            <AccordionItem key={index} className="py-4" value={`item-${index + 1}`}>
-              <AccordionTrigger className="text-2xl text-start font-semibold">
-                {item.question}
-              </AccordionTrigger>
-              <AccordionContent className="text-lg font-medium">
-                {item.answer}
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </div>
-    </section>
-  );
-};
+interface Question {
+    question: string;
+    answer: string;
+  }
+  
+  export const Faq = ({ className }: Props) => {
+    const t = useTranslations("Main.Faq");
+  
+    const questions: Question[] = t.raw("questions"); // Указываем правильный тип
+  
+    return (
+      <section className={cn("bg-white py-24", className)}>
+        <div className="w-full max-w-[1500px] px-4 mx-auto flex flex-col gap-12">
+          <h3 className="text-3xl lg:text-4xl font-bold">{t("title")}</h3>
+          <Accordion type="single" collapsible className="w-full">
+            {questions.map((item, index) => (
+              <AccordionItem key={index} className="py-4" value={`item-${index + 1}`}>
+                <AccordionTrigger className="text-2xl text-start font-semibold">
+                  {item.question}
+                </AccordionTrigger>
+                <AccordionContent className="text-lg font-medium">
+                  {item.answer}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
+      </section>
+    );
+  };
--- components/shared/Main/Plan/Plan.tsx
+++ components/shared/Main/Plan/Plan.tsx
@@ -6,28 +6,31 @@ interface Props {
   className?: string;
 }
 
+interface Module {
+  name: string;
+  description: string;
+}
+
 export const Plan = ({ className }: Props) => {
   const t = useTranslations("Main.EducationPlan");
 
-  const modules = t.raw("modules"); // Получаем массив модулей из локализации
+  const modules: Module[] = t.raw("modules"); // Указываем правильный тип
 
   return (
     <section className={cn("bg-white py-24", className)}>
       <div className="w-full max-w-[1500px] px-4 mx-auto flex gap-8 flex-col">
         <div className="space-y-4">
           <h3 className="text-3xl lg:text-4xl font-bold">{t("title")}</h3>
-          <p className="font-medium text-lg">
-            {t("description")}
-          </p>
+          <p className="font-medium text-lg">{t("description")}</p>
         </div>
         <div>
           <hr />
-          {modules.map((module: any, index: number) => (
+          {modules.map((module, index) => (
             <div key={index}>
               <div className="my-8 flex max-mdx:flex-col gap-6">
                 <div className="flex-1 flex flex-col gap-1">
                   <h6 className="text-lg text-[#009FE3] font-semibold flex items-center">
-                    {`${t('module')} ${index + 1}`}
+                    {`${t("module")} ${index + 1}`}
                   </h6>
                   <h4 className="text-3xl font-bold w-full max-w-[400px]">
                     {module.name}
